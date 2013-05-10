fca17863fe5a3dd3f59c98910b9b23637c77e1df
rhannequin <remy.hannequin@gmail.com>
2013-05-10 17:28:34 +0200

    Display users list

--- js/init.js
+++ js/init.js
@@ -2,8 +2,14 @@ require.config({
   baseUrl: '/',
   deps: ['js/main'],
   paths: {
-    jquery:    'libs/jquery-2.0.0',
-    bootstrap: 'libs/bootstrap',
-    text:      'libs/text'
+    jquery:     'libs/jquery-2.0.0',
+    bootstrap:  'libs/bootstrap',
+    underscore: 'libs/underscore',
+    text:       'libs/text'
+  },
+  shim: {
+    underscore: {
+      exports: '_'
+    }
   }
 });
